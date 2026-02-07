524e0b3260597b4e8aadb43e35455705b79a1695
AndrewIdot <78974994+AndrewIdot@users.noreply.github.com>
2026-02-07 08:16:29 +0200

    romaniastuck rehost (upto 8)

--- comics/js/pageload.js
+++ comics/js/pageload.js
@@ -1,6 +1,24 @@
 var params = new URLSearchParams(document.location.search);
 var p = params.get("p");
+p = Number(p);
+
+if (!p) {
+	p = 1;
+}
 
 document.getElementById(`page${p}`).className = "";
 
-document.getElementsByClassName("scaryhidden").remove();
+function removeElementsByClass(className){
+    const elements = document.getElementsByClassName(className);
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
+removeElementsByClass("scaryhidden");
+
+var yourElement = document.getElementById('gobackpage');
+yourElement.setAttribute('href', '?p=' + (p - 1));
+
+var yourElementp = document.getElementById('gofrontpage');
+yourElementp.setAttribute('href', '?p=' + (p + 1));
